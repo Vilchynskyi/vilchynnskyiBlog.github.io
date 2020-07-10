916716cb071626300058b18805de604c4256cf6a
Vilya22 <roma.vilchynskyy@gmail.com>
2020-07-10 21:04:01 +0300

    scroll nav and  hamburger menu added

--- app.js
+++ app.js
@@ -23,3 +23,46 @@ $(document).ready(function(){
         }
     );
 });
+
+
+// burger menu
+
+let mobileMenu = document.querySelector(".mobile_menu");
+let mainMenu = document.querySelector(".navigation");
+
+mobileMenu.addEventListener("click", function() {
+	mobileMenu.classList.toggle("active_menu");
+	if (mobileMenu.classList.contains("active_menu")) {
+		mainMenu.classList.add("active_menu");
+	} else {
+		mainMenu.classList.remove("active_menu")
+	}
+})
+
+let menuItem = document.querySelectorAll(".menu_item")
+for (i=0; i < menuItem.length; i++) {
+	menuItem[i].addEventListener("click", function() {
+		mobileMenu.classList.remove("active_menu");
+		mainMenu.classList.remove("active_menu");
+	})
+}
+
+// scroll
+
+$('#nav').onePageNav({
+	currentClass: 'current',
+	changeHash: false,
+	scrollSpeed: 750,
+	scrollThreshold: 0.5,
+	filter: '',
+	easing: 'swing',
+	begin: function() {
+		//I get fired when the animation is starting
+	},
+	end: function() {
+		//I get fired when the animation is ending
+	},
+	scrollChange: function($currentListItem) {
+		//I get fired when you enter a section and I pass the list item of the section
+	}
+});
